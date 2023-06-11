5854d9543877ecd56dfd6b700ac01ed2145abc29
feby <83932271+febyfadlilah@users.noreply.github.com>
2023-06-11 11:24:47 +0700

    Update analisis_sentimen.py

--- analisis_sentimen.py
+++ analisis_sentimen.py
@@ -1,12 +1,8 @@
 import streamlit as st
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn import svm
 import requests
 import io
 import string
-import pandas as pd
 import re
 import nltk
 from nltk.corpus import stopwords
@@ -14,9 +10,12 @@ from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
 from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
 import pickle
+from sklearn import svm
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 import altair as alt
 from PIL import Image
+
 image = Image.open('pilpres.jpg')
 
 nltk.download('punkt')
@@ -49,20 +48,6 @@ def preprocess_data(text):
 
     return text
 
-# Load the selection features from the pickle file
-with open('selected_features.pickle', 'rb') as file:
-    selected_features = pickle.load(file)
-
-# Function to train the SVM model
-def train_svm(X_train, y_train, test_size, kernel):
-    vectorizer = TfidfVectorizer(tokenizer=preprocess_data)
-    X_train = vectorizer.fit_transform(X_train)
-    X_train = X_train[selected_features]
-    X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=test_size, random_state=42)
-    clf = svm.SVC(kernel=kernel)
-    clf.fit(X_train, y_train)
-    return clf, vectorizer
-
 # Function to make sentiment predictions
 def predict_sentiment(text, clf, vectorizer):
     text = vectorizer.transform([text])
@@ -72,23 +57,25 @@ def predict_sentiment(text, clf, vectorizer):
 # Load the dataset from GitHub
 data = pd.read_excel("https://raw.githubusercontent.com/febyfadlilah/dataset/main/DataCrawlingTwitter2019Label.xlsx")
 
+# Load the selection features from the pickle file
+with open('selected_features.pickle', 'rb') as file:
+    selected_features = pickle.load(file)
+
 # Split features and labels
 X_train = data['Text']
 y_train = data['Label']
 
-# Muat parameter terbaik dari file pickle
-with open('best_params.pkl', 'rb') as file:
-    best_params = pickle.load(file)
+# Transform the text data using TF-IDF with selected features
+vectorizer = TfidfVectorizer(tokenizer=preprocess_data, vocabulary=selected_features)
+X_train = vectorizer.fit_transform(X_train)
 
-best = best_params[0]
+# Train the SVM model
+clf = svm.SVC()
+clf.fit(X_train, y_train)
 
-# Ambil parameter terbaik
-best_test_size = best[0]
-best_kernel = best[1]
-
-# Train the SVM model using the best parameters
-clf, vectorizer = train_svm(X_train, y_train, best_test_size, best_kernel)
-
+# Save the trained model to a pickle file
+with open('svm_model.pickle', 'wb') as file:
+    pickle.dump(clf, file)
 
 # Streamlit app title
 st.title('Analisis Sentimen Tweets Pilpres 2019')
@@ -100,7 +87,10 @@ input_text = st.text_input('Masukkan teks:', '')
 # Button to perform sentiment prediction
 if st.button('Prediksi Sentimen'):
     if input_text:
-        prediction = predict_sentiment(input_text, clf, vectorizer)
-        st.write('Prediksi sentimen:',prediction)
+        processed_text = preprocess_data(input_text)
+        processed_text = ' '.join(processed_text)
+        processed_text = vectorizer.transform([processed_text])
+        prediction = clf.predict(processed_text)
+        st.write('Prediksi sentimen:', prediction[0])
     else:
         st.write('Silakan masukkan teks untuk melakukan prediksi.')
